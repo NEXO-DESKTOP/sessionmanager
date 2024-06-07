22bc34da41a3a2494ae1230c64be28a8184daae2
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2024-06-07 18:50:44 +0200

    a lot of ui stuff

--- resources/js/main.js
+++ resources/js/main.js
@@ -18,7 +18,7 @@ document.getElementById('minimize').addEventListener('click', function() {
 
 document.getElementById('shutdown').addEventListener('click', function() {
     let button = Neutralino.os.showMessageBox('Apagar',
-                            '¿Quieres apagar el sistema? Todas las apps se cerrarán y se perderán los datos no guardados.',
+                            '¿Quieres apagar el sistema?\nTodas las apps se cerrarán y se perderán los datos no guardados.',
                             'YES_NO', 'WARNING');
         if(button == 'YES') {
             Neutralino.app.exit();
@@ -28,7 +28,7 @@ document.getElementById('shutdown').addEventListener('click', function() {
 
 document.getElementById('restart').addEventListener('click', function() {
     let button = Neutralino.os.showMessageBox('Reiniciar',
-                            '¿Quieres reiniciar el sistema? Todas las apps se cerrarán y se perderán los datos no guardados.',
+                            '¿Quieres reiniciar el sistema?\nTodas las apps se cerrarán y se perderán los datos no guardados.',
                             'YES_NO', 'WARNING');
         if(button == 'YES') {
             Neutralino.app.exit();
@@ -41,3 +41,19 @@ document.getElementById('appshow').addEventListener('click', function() {
      window.location.href = '/apps.html';
 }
 );
+
+let nativaPersona = Neutralino.os.execCommand("getent passwd $USER | cut -d: -f5 | tr ',' ' '");
+nativaPersona.then(result => {
+    let usersession = result.stdOut
+    console.log(usersession);
+    const username = document.getElementsByClassName("username");
+    for(let i = 0; i < username.length; i++) {
+        username[i].textContent = usersession;
+    }
+
+    Neutralino.window.setTitle(usersession);
+    
+}).catch(error => {
+    console.error(error); // Esto manejará cualquier error que pueda ocurrir
+});
+
